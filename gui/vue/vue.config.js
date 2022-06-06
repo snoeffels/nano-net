f1c456be1990abaa3ca3d0b40607f034ea68f952
Dennis Borrmann <debbo@hotmail.de>
2022-06-06 17:03:41 +0200

    - python and js are now talking via functions

--- gui/vue/vue.config.js
+++ gui/vue/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+require('path');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../dist"
 })
